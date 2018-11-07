13a00abea41d36889d4d57d4fa6b1cc2e52232cc
Avinash <avinashdhillor24@gmail.com>
2018-11-08 02:59:41 +0530

    Project Complted with react

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      quote: '',
+      author: ''
+    };
+    this.isClicked = this.isClicked.bind(this);
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  componentWillMount() {
+    fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let random = this.getRandomInt(data.quotes.length);
+        this.setState(() => ({
+          quotes: data.quotes,
+          quote: data.quotes[random].quote,
+          author: data.quotes[random].author
+        }));
+      })
+      .catch(err => {});
+  }
+
+  isClicked() {
+    let random = this.getRandomInt(this.state.quotes.length);
+    this.setState(() => ({
+      quote: this.state.quotes[random].quote,
+      author: this.state.quotes[random].author
+    }));
+  }
+
+  render() {
+    return (
+      <div className="box-wrapper">
+        <div className="box-background">
+          <div className="box-content">
+            <h1>"{this.state.quote}"</h1>
+            <p>-{this.state.author}</p>
+            <Action passed={this.isClicked} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Action = props => {
+  return (
+    <div class="box-button__next">
+      <button className="twitter">Twitter</button>
+      <button className="github">Facebook</button>
+      <button className="next" onClick={props.passed}>
+        New Quote
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Display />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public', 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/'
+  }
+};
