94f8ac78de5aed8704e02be3a712f1f279599993
Avinash Dhillor <avinashdhillor24@gmail.com>
2019-02-11 23:25:31 +0530

    code cleaned

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.scss";
 
 class Display extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quotes: [],
-      quote: '',
-      author: ''
+      quote: "",
+      author: ""
     };
     this.isClicked = this.isClicked.bind(this);
   }
@@ -20,7 +19,7 @@ class Display extends React.Component {
 
   componentWillMount() {
     fetch(
-      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     )
       .then(response => {
         return response.json();
